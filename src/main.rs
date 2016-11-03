7904420b3a8e284a360658334849a6e2cedc800d
Joshua Rombauer <joshua.rombauer@tum.de>
2016-11-03 09:33:40 +0100

    implements getchallenge

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,21 @@ fn to_hex_string(bytes: &[u8]) -> String {
 }
 
 fn main() {
-    
-    const PREFIX: &'static [u8; 4] = b"\xFF\xFF\xFF\xFF";
-    const LISTEN: &'static str = "127.0.0.1:5555";
-    const HOST: &'static str = "94.23.7.172:27960";
 
-    let mut getstatus : Arc<(u32, &mut [u8])> = Arc::new((0, &mut []));
-    let mut getinfo : Arc<(u32, &mut [u8])> = Arc::new((0, &mut []));
-    
+    const PREFIX: &'static [u8; 4] = b"\xFF\xFF\xFF\xFF";
+    const LISTEN: &'static str = "0.0.0.0:5555";
+    const HOST: &'static str = "94.23.7.172:27960";
+    const CHALLENGERESPONSE: &'static [u8] = b"\xFF\xFF\xFF\xFFprint\nET://94.23.7.172:27960";
+
+    let mut getinfo: Arc<(u32, &mut [u8])> = Arc::new((0, &mut []));
+
     println!("Spinning up server.");
     let socket = match UdpSocket::bind(LISTEN) {
         Ok(s) => {
             println!("Listening on: {}", s.local_addr().unwrap());
             s
-        },
-        Err(e) => panic!("Could not bind socket: {}", e)
+        }
+        Err(e) => panic!("Could not bind socket: {}", e),
     };
 
     let mut buf = [0; 2048];
@@ -35,28 +35,29 @@ fn main() {
     loop {
         match socket.recv_from(&mut buf) {
             Ok((amt, src)) => {
-                if buf[0 .. 4] != *PREFIX {
+                if buf[0..4] != *PREFIX {
                     println!("Invalid Prefix");
                     continue;
                 };
+                let sock = socket.try_clone().unwrap();
                 thread::spawn(move || {
                     println!("new thread Spawned.");
                     println!("amt: {}", amt);
                     println!("src: {}", src);
                     println!("{}", to_hex_string(&buf));
 
-                    let s = str::from_utf8(&buf[4 .. amt]).unwrap_or("Invalid str");
+                    let s = str::from_utf8(&buf[4..amt]).unwrap_or("Invalid str");
                     println!("{}", s);
 
                     match s {
                         s if s.starts_with("getinfo") => unimplemented!(),
                         s if s.starts_with("getstatus") => unimplemented!(),
-                        s if s.starts_with("getchallenge") => unimplemented!(),
-                        _ => panic!("Invalid request type")
+                        s if s.starts_with("getchallenge") => sock.send_to(CHALLENGERESPONSE, src),
+                        _ => panic!("Invalid request type"),
                     }
                 });
-            },
-            Err(e) => println!("Could not receive a packet: {}", e)
+            }
+            Err(e) => println!("Could not receive a packet: {}", e),
         }
     }
 }
